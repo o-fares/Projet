2c9271b3cde940ddccd4cf3ac408d682e9103e62
B-Mounir <mounirb13@gmail.com>
2017-04-04 21:49:41 +0200

    Add files via upload
    Vérifier si elles marchent.

--- Arbre.py
+++ Arbre.py
@@ -1,1 +1,52 @@
-# classe Arbre
+# Author : Mounir Bennadji
+# File : Classe Arbre.py
+
+from Noeud import *
+
+class Arbre():
+
+    def __init__(self, A1, n, A2):
+        self.fg = A1
+        self.fd = A2
+        self.racine =  n
+
+    def estVide(self):
+        """Renvoie True si l'arbre est vide, False sinon"""
+        return (self.fd == None) and (self.fg == None) and (self.racine == None)
+
+    def setNoeud(self,A):
+        self.racine = A
+
+    def setFd(self,A):
+        self.fd = A
+
+    def setFg(self,B):
+        self.fg = B
+
+    def getFd(self):
+        """Retourne la valeur du fils droit """
+        assert (not self.estVide())
+        return self.fd
+
+    def getFg(self):
+        """Retourne la valeur du fils gauche"""
+        assert (not self.estVide())
+        return self.fg
+
+    def getRacine(self):
+        """Renvoie la racine de l'arbre"""
+        assert(not self.estVide())
+        return self.racine
+
+    def getValRac(self):
+        """Retourne l'étiquette de la racine"""
+        assert (not self.estVide())
+        return self.racine.getVal()
+
+    def estFeuille(self):
+        """Vérifie si un arbre est une feuille"""
+        if self.estVide():
+            return False
+        else :
+            return self.fd.estVide() and self.fg.estVide()
+
--- testArbre.py
+++ testArbre.py
@@ -1,1 +1,37 @@
-# classe de tests Arbre
+
+
+import unittest
+
+from Noeud import *
+from Arbre import *
+from copy import *
+
+class TestArbre(unittest.TestCase):
+
+    def setUp(self) :
+        self.node0 = Noeud(0)
+        self.node1 = Noeud(1)
+        self.node2 = Noeud(2)
+        self.av1 = Arbre(None,None,None)
+        self.av2 = Arbre(None,None,None)
+        self.av3 = Arbre(None,None,None)
+        self.a3 = (self.av1, self.node1, self.av2)
+        self.a4 = (deepcopy(self.av1), self.node2, deepcopy(self.av2))
+
+    def testEstVide1(self):
+        self.assertTrue(self.av1.estVide())
+
+    def testEstNonVide(self):
+        self.a2 = (None, self.node0,None)
+        self.assertFalse(self.a3.estVide())
+
+    def testEstFeuille(self):
+        self.assertTrue(self.a3.estFeuille())
+
+    def testEstNonFeuille(self):
+        a = Arbre(self.a3, self.node2, self.a4)
+        self.assertFalse(a.estFeuille())
+
+
+if __name__ == '__main__':
+    unittest.main()
