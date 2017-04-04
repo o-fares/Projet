cbf689f788b2b5a8429672de24f41587fd3e019a
Fares <ofares97@live.fr>
2017-04-04 22:21:12 +0200

    Update Paire.py

--- Paire.py
+++ Paire.py
@@ -2,23 +2,31 @@
 class Paire():
     #Classe des points du plan
     def __init__(self,a,b):
-        self.elemnent = a
-        self.priorité = b
+        self.element = a
+        self.priorite = b
+        
     def getX(self):
-        #Première élément de la paire
+        #Premier élément de la paire
         return self.element
+    
     def getY(self):
-        return self.priorité
-    def setX(self,x):
+        return self.priorite
+    
+    def setX(self, x):
         #Modification de la première valeur de la paire
         self.element = x
-    def setY(self,y):
+        
+    def setY(self, y):
         #Modification de la deuxième valeur de la paire
-        self.priorité = y
-    def eqpaire(q,p):
+        self.priorite = y
+        
+    def eqPaire(q, p):
         #Renvoie True si les deux paires sont égales, False sinon
         return (q.getX() == p.getX()) and (q.getY() == p.getY())
+    
     def afficher(self):
+        """affiche la paire"""
         return (self.getX(), self.getY())
+    
     def cloner(self):
         return (Paire(self.getX(),self.getY()))
